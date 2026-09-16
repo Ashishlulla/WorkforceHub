d913b52caf3a9c66df9d60e19d166a62767ec467
Ashish Lulla <lullaashish2807@gmail.com>
2026-09-16 12:43:00 +0530

    Initialize WorkForceHub solution and architecture

---
+++ WorkForceHub.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkForceHub.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ WorkForceHub.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkForceHub.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
